73064db9efd5a318d8c8c888164d0fb65a0c3a16
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-01-25 13:44:09 +0100

    Comentario

--- bisiesto.py
+++ bisiesto.py
@@ -1,6 +1,10 @@
 #coding: utf-8
-anio=input("Indica un año:")
-if (anio%4==0 and anio%100!=0) or anio%400==0:
-	print anio,"es visiesto"
-else:
-	print anio,"no es bisiesto"
+def esBisiesto(anio):
+	if (anio%4==0 and anio%100!=0) or anio%400==0:
+		print anio,"es visiesto"
+	else:
+		print anio,"no es bisiesto"
+esBisiesto(2000)
+esBisiesto(2002)
+esBisiesto(2003)
+
